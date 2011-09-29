eb48f817b112aca99f6673865d01adc465dcb733
Pablo Molina <faival@gmail.com>
2011-09-29 12:47:01 -0700

    changed name of classes

---
+++ src/utils/GitUtilities.java
@@ -0,0 +1,144 @@
+package utils;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.log4j.Logger;
+
+import siena.Json;
+
+public class GitUtilities {
+	public static Logger logger = Logger.getLogger(GitUtilities.class);	
+
+	public static String getString(BufferedReader bR) {
+		String s = null;
+		String out = "";
+		try {
+			while ((s = bR.readLine()) != null) {
+				out += s;
+			}
+			return out;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static String piped2String(PipedInputStream pis){
+		final PipedOutputStream pos = new PipedOutputStream();
+		final BufferedInputStream bis = new BufferedInputStream(pis);
+		final BufferedOutputStream bos = new BufferedOutputStream(pos);
+		final StringBuffer sb = new StringBuffer();
+		int p = 0;
+		byte[] b = new byte[1024];
+		try {
+			while (!(bis.available()<1)){
+				p = bis.read(b, 0, 1024);
+				bos.write(b, 0 , p);
+				sb.append(new String(b, 0 , p));
+			}
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+			ioe.getMessage();
+		}
+		String content = sb.toString();
+		return content;
+	}
+	
+	public static Json parseData(BufferedReader stdOutput,  String stdErr, int statusCode, String action){
+		// status code NOK
+		if (statusCode != 0)
+			return Json.map().put("status", "NOK").put("action", action).put("error", stdErr);
+
+		// Log or incoming need parsing
+		if (GitConstants.LOG.equals(action) || GitConstants.INCOMING.equals(action)){
+			Pattern commitPattern = Pattern.compile(GitConstants.LOG_PATTERN);
+			String s = null;
+			Json logList = Json.list();
+			String tmp="";
+			try{
+				//Deal with multiline comments and commitlogs
+				while( ( s = stdOutput.readLine()) != null){
+					s = s.trim();
+					if(s.startsWith("[BEGIN]") && !s.endsWith("EOC")){
+						tmp = s.replace("\n", " ");
+						continue;
+					}else if(!s.startsWith("[BEGIN]") && !s.endsWith("EOC") && tmp.startsWith("[BEGIN]")){
+						tmp += s.replace("\n", " ");
+						continue;
+					}else if (!s.startsWith("[BEGIN]") && s.endsWith("EOC") && tmp.startsWith("[BEGIN]")){
+						tmp += s.replace("\n", " ");
+						s = tmp;
+					}
+					tmp="";
+					Matcher logM = commitPattern.matcher(s);
+					if(logM.find()){
+						Json rev = processRevisionLog(logM);
+						logList.add(rev);
+					}
+				}
+			} catch (Exception e) {
+				return(Json.map().put("status", "NOK").put("action", action).put("error", "exception").put("message", e.getMessage()));
+			}
+			return Json.map().put("status", "OK").put("commits", logList);
+		}
+		//everything else is OK
+		return Json.map().put("status", "OK" );
+	}
+
+	private static Json processRevisionLog(Matcher rev) {
+		String revision = rev.group(1);
+		String node = rev.group(2);
+		String author = rev.group(3);
+		String date = rev.group(4);
+		String message = rev.group(5);	
+		String added = rev.group(6);
+		String removed = rev.group(7);
+		String files = rev.group(8);
+		Json revLog = Json.map()
+		.put("revision", revision)
+		.put("node", node)
+		.put("author", author)
+		.put("date", date)
+		.put("message", message);
+
+		List <String >addedList = Arrays.asList(added.split("\\s+"));
+		List <String >removedList = Arrays.asList(removed.split("\\s+"));
+		List <String> modifiedList = new ArrayList<String>();
+		List <String >fileList = Arrays.asList(files.split("\\s+"));
+		for (String string : fileList) {
+			if(!addedList.contains(string) || !removedList.contains(string)){
+				modifiedList.add(string);
+			}
+		}
+		revLog.put("added", addedList)
+		.put("removed", removedList)
+		.put("modified", modifiedList);
+		return revLog;
+	}
+	
+		static public boolean deleteDirectory(File path) {
+			if( path.exists() ) {
+				File[] files = path.listFiles();
+				for(int i=0; i<files.length; i++) {
+					if(files[i].isDirectory()) {
+						deleteDirectory(files[i]);
+					}
+					else {
+						files[i].delete();
+					}
+				}
+			}
+			return( path.delete() );
+		}
+}
---
+++ src/wrapper/GitConnector.java
@@ -0,0 +1,184 @@
+package wrapper;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PrintStream;
+import java.util.UUID;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.Executor;
+import org.apache.commons.exec.PumpStreamHandler;
+import org.apache.log4j.Logger;
+
+import siena.Json;
+import utils.GitConstants;
+import utils.GitUtilities;
+import exceptions.RepositoryNotFoundException;
+
+
+public class GitConnector {
+	public static Logger logger = Logger.getLogger(GitConnector.class);	
+	private String command = "/usr/bin/hg";
+	private String uri;
+	private String path;
+	private String revFrom = "";
+	private String revTo = "";
+
+	public GitConnector(String uri, String path, String command){
+		this.command = command;
+		this.uri = uri;
+		this.path = path;
+	}
+
+	public GitConnector(String uri, String path){
+		this.uri = uri;
+		this.path = path;
+	}
+	
+	// main call to system
+	private Json callHG (String action) throws RepositoryNotFoundException{
+			File dir = new File(path);
+
+		CommandLine cl = new CommandLine(command);
+		logger.debug("HGConnector msg: Starting to "+action+" over "+this.uri);
+		if (GitConstants.LOG.equals(action) || "incoming".equals(action)){
+			if (!dir.exists() || dir.list().length == 0){
+				throw new RepositoryNotFoundException(path);
+			}
+			cl.addArgument(action);
+			cl.addArgument("--rev");
+			cl.addArgument(revFrom+":"+revTo);
+			cl.addArgument("--template");
+			cl.addArgument(GitConstants.TEMPLATE);
+		} else if ("cloneRev".equals(action)) {
+			if (!dir.exists()) {
+				dir.mkdir();
+			}
+			cl.addArgument(GitConstants.CLONE);
+			cl.addArgument("--rev");
+			cl.addArgument(revFrom);
+			cl.addArgument(uri);
+			cl.addArgument(path);
+		} else if (GitConstants.CLONE.equals(action)) {
+			if (!dir.exists()){ 
+				logger.debug(dir.getAbsolutePath());
+				logger.debug("do not exists");
+				dir.mkdir();
+			}
+			cl.addArgument(GitConstants.CLONE);
+			cl.addArgument(uri);
+			cl.addArgument(path);
+		} else if (GitConstants.PULL.equals(action)){
+			if(!dir.exists() || dir.list().length == 0 ){
+				return callHG(GitConstants.CLONE);
+			}else if(dir.list().length ==1 && dir.list()[0].equals(".hg")){
+				//some times the clone corrupts so we just get a .hg dir but not working local repo
+				//delete it and try a clean clone
+				logger.info("deleting"+ dir.getAbsolutePath());
+				GitUtilities.deleteDirectory(dir);
+				return callHG(GitConstants.CLONE);
+			}
+			cl.addArgument(GitConstants.PULL);
+			cl.addArgument("-u");
+		}
+		Json result = Json.map();
+		
+		File file = null;
+		FileOutputStream fOS = null;
+		FileReader fr = null;
+		BufferedReader br = null;
+		PipedOutputStream pipeOut = null;
+		PipedInputStream pipeIn = null;
+		
+		try{
+			file = File.createTempFile("HG-", ".log");
+			fOS = new FileOutputStream(file);
+			PumpStreamHandler streamHandler = new PumpStreamHandler();
+			streamHandler = new PumpStreamHandler(fOS);
+			Executor executor = new DefaultExecutor();
+			executor.setStreamHandler(streamHandler);
+			executor.setWorkingDirectory(dir);
+			logger.debug(cl.toString());
+			
+			// redirecting System.err and prepare a pipeIn to read it
+			pipeOut = new PipedOutputStream();
+			pipeIn = new PipedInputStream(pipeOut);
+			System.setErr (new PrintStream(pipeOut));
+			
+			/* 	DefaultExecutor only accepts one value as success value
+				by default, we need to accept more and discriminate the 
+				result afterwards. So Don't Panic
+			*/
+			int [] dontPanicValues = new int[256]; 
+			for (int i = 0; i <= 255; i++) {
+				dontPanicValues[i]=i;
+			}
+			executor.setExitValues(dontPanicValues);
+			
+			//execute command
+			int statusCode = executor.execute(cl);
+			logger.debug("HGConnector msg: Executed "+action+" over "+this.uri+ " exitStatus "+statusCode);
+			fr = new FileReader(file);
+			br = new BufferedReader(fr);
+			String stdErr = GitUtilities.piped2String(pipeIn);
+			result = GitUtilities.parseData(br, stdErr, statusCode, action);
+			logger.debug("HGConnector msg: result "+ result);
+			
+		}
+		catch (IOException e) {
+			logger.error("HG Exception: "+ uri, e);
+		} finally {
+			try {fOS.close();} catch (Exception e) {}
+			try {fr.close();} catch (Exception e) {}
+			try {br.close();} catch (Exception e) {}
+			try {pipeOut.close();} catch (Exception e) {}
+			try {pipeIn.close();} catch (Exception e) {}
+			logger.debug("File size " + file.length());
+			file.delete();
+		}
+		return result;
+	}
+	
+	// public methods
+	//clones
+	
+	public Json cloneRev(String revision) throws RepositoryNotFoundException{
+		this.revFrom = revision;
+		return callHG("cloneRev");
+	}
+
+	public Json cloneRepo() throws RepositoryNotFoundException{
+		return callHG("clone");
+	}
+	
+	// logs
+	public Json log() throws RepositoryNotFoundException{
+		return callHG("log");
+	}
+	public Json log(String revFrom, String revTo) throws RepositoryNotFoundException{
+		this.revFrom = revFrom;
+		this.revTo = revTo;
+		return  callHG("log");		
+	}
+	
+	public Json log(String revFrom) throws RepositoryNotFoundException{
+		this.revFrom = revFrom;
+		this.revTo="tip";
+		return callHG("log");
+	}
+	
+	public Json hgRemoteLog() throws RepositoryNotFoundException{
+		return callHG("incoming");
+	}
+	
+	//update
+	public Json pull() throws RepositoryNotFoundException{
+		return callHG("pull");
+	}
+}
+
